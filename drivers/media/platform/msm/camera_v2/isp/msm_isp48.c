5c99d608114455a6c220215828dbeb1497e3806b
Shubhraprakash Das <sadas@codeaurora.org>
2016-04-06 21:36:56 -0700

    msm: camera: isp: Set vfe clock rate
    Set the initial vfe clock rate to SVS during probe. When the
    clock is enabled the clock driver expects a clock rate to be
    voted. The clock is enabled during device open and the
    user driver can send the actual rate only after open. This causes
    a warning print from clock driver when the clock is enabled in
    device open. To avoid the error print set an initail rate.
    
    CRs-Fixed: 1000316
    Signed-off-by: Shubhraprakash Das <sadas@codeaurora.org>
    Change-Id: I7c2681bb73e8580722b746ff3da6be3ac0e9c518
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp48.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp48.c
@@ -250,21 +250,6 @@ static int msm_vfe48_get_clks(struct vfe_device *vfe_dev)
 				vfe_dev->vfe_clk_rates
 					[MSM_VFE_CLK_RATE_SVS][i]);
 		}
-		if (strcmp(vfe_dev->vfe_clk_info[i].clk_name,
-					"mnoc_maxi_clk") == 0)
-			vfe_dev->vfe_clk_info[i].clk_rate = INIT_RATE;
-		/* set no memory retention */
-		if (strcmp(vfe_dev->vfe_clk_info[i].clk_name,
-				"camss_vfe_clk") == 0 ||
-			strcmp(vfe_dev->vfe_clk_info[i].clk_name,
-				"camss_csi_vfe_clk") == 0 ||
-			strcmp(vfe_dev->vfe_clk_info[i].clk_name,
-				"camss_vfe_vbif_axi_clk") == 0) {
-			msm_camera_set_clk_flags(vfe_dev->vfe_clk[i],
-				 CLKFLAG_NORETAIN_MEM);
-			msm_camera_set_clk_flags(vfe_dev->vfe_clk[i],
-				 CLKFLAG_NORETAIN_PERIPH);
-		}
 	}
 	return 0;
 }
