cc3528b8f802fab0370a4bdde3735c7bfa1ca615
Artem Borisov <dedsa2002@gmail.com>
2018-10-28 17:05:49 +0400

    drivers: video: mdss: Convert to wakeup sources
    -e
    
    Change-Id: Ifa9d5cb79ffccce1c5263a29f6100eb3103ad902

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -46,6 +46,9 @@
 #include <linux/file.h>
 #include <linux/kthread.h>
 #include <linux/dma-buf.h>
+#if defined(CONFIG_MACH_XIAOMI_MSM8953) || defined(CONFIG_MACH_XIAOMI_MARKW)
+#include <linux/mdss_io_util.h>
+#endif
 #include "mdss_fb.h"
 #include "mdss_mdp_splash_logo.h"
 #define CREATE_TRACE_POINTS
@@ -121,6 +124,36 @@ static int mdss_fb_send_panel_event(struct msm_fb_data_type *mfd,
 					int event, void *arg);
 static void mdss_fb_set_mdp_sync_pt_threshold(struct msm_fb_data_type *mfd,
 		int type);
+#if defined(CONFIG_MACH_XIAOMI_MSM8953) || defined(CONFIG_MACH_XIAOMI_MARKW)
+#define WAIT_RESUME_TIMEOUT 200
+struct fb_info *prim_fbi;
+static struct delayed_work prim_panel_work;
+static atomic_t prim_panel_is_on;
+static struct wakeup_source prim_panel_wakelock;
+static void prim_panel_off_delayed_work(struct work_struct *work)
+{
+#ifdef CONFIG_FRAMEBUFFER_CONSOLE
+	console_lock();
+#endif
+	if (!lock_fb_info(prim_fbi)) {
+#ifdef CONFIG_FRAMEBUFFER_CONSOLE
+		console_unlock();
+#endif
+		return;
+	}
+	if (atomic_read(&prim_panel_is_on)) {
+		fb_blank(prim_fbi, FB_BLANK_POWERDOWN);
+		atomic_set(&prim_panel_is_on, false);
+		__pm_relax(&prim_panel_wakelock);
+	}
+
+	unlock_fb_info(prim_fbi);
+#ifdef CONFIG_FRAMEBUFFER_CONSOLE
+	console_unlock();
+#endif
+}
+#endif
+
 void mdss_fb_no_update_notify_timer_cb(unsigned long data)
 {
 	struct msm_fb_data_type *mfd = (struct msm_fb_data_type *)data;
@@ -1473,6 +1506,14 @@ static int mdss_fb_remove(struct platform_device *pdev)
 
 	mdss_fb_remove_sysfs(mfd);
 
+#if defined(CONFIG_MACH_XIAOMI_MSM8953) || defined(CONFIG_MACH_XIAOMI_MARKW)
+	if (mfd->panel_info && mfd->panel_info->is_prim_panel) {
+		atomic_set(&prim_panel_is_on, false);
+		cancel_delayed_work_sync(&prim_panel_work);
+		wakeup_source_trash(&prim_panel_wakelock);
+	}
+#endif
+
 	pm_runtime_disable(mfd->fbi->dev);
 
 	if (mfd->key != MFD_KEY)
@@ -2169,6 +2210,16 @@ static int mdss_fb_blank(int blank_mode, struct fb_info *info)
 	ktime_t start, end;
 	s64 actual_time;
 
+#if defined(CONFIG_MACH_XIAOMI_MSM8953) || defined(CONFIG_MACH_XIAOMI_MARKW)
+	if ((info == prim_fbi) && (blank_mode == FB_BLANK_UNBLANK) &&
+		atomic_read(&prim_panel_is_on)) {
+		atomic_set(&prim_panel_is_on, false);
+		__pm_relax(&prim_panel_wakelock);
+		cancel_delayed_work_sync(&prim_panel_work);
+		return 0;
+	}
+#endif
+
 	start = ktime_get();
 	ret = mdss_fb_pan_idle(mfd);
 	if (ret) {
@@ -2854,6 +2905,14 @@ static int mdss_fb_register(struct msm_fb_data_type *mfd)
 	mdss_panel_debugfs_init(panel_info, panel_name);
 	pr_info("FrameBuffer[%d] %dx%d registered successfully!\n", mfd->index,
 					fbi->var.xres, fbi->var.yres);
+#if defined(CONFIG_MACH_XIAOMI_MSM8953) || defined(CONFIG_MACH_XIAOMI_MARKW)
+	if (panel_info->is_prim_panel) {
+		prim_fbi = fbi;
+		atomic_set(&prim_panel_is_on, false);
+		INIT_DELAYED_WORK(&prim_panel_work, prim_panel_off_delayed_work);
+		wakeup_source_init(&prim_panel_wakelock, "prim_panel_wakelock");
+	}
+#endif
 
 	return 0;
 }
@@ -5257,6 +5316,63 @@ void mdss_fb_report_panel_dead(struct msm_fb_data_type *mfd)
 		KOBJ_CHANGE, envp);
 	pr_err("Panel has gone bad, sending uevent - %s\n", envp[0]);
 }
+
+#if defined(CONFIG_MACH_XIAOMI_MSM8953) || defined(CONFIG_MACH_XIAOMI_MARKW)
+/*
+ * mdss_prim_panel_fb_unblank() - Unblank primary panel FB
+ * @timeout : >0 blank primary panel FB after timeout (ms)
+ */
+int mdss_prim_panel_fb_unblank(int timeout)
+{
+	int ret = 0;
+	struct msm_fb_data_type *mfd = NULL;
+
+	if (prim_fbi) {
+		mfd = (struct msm_fb_data_type *)prim_fbi->par;
+		ret = wait_event_timeout(mfd->resume_wait_q,
+				!atomic_read(&mfd->resume_pending),
+				msecs_to_jiffies(WAIT_RESUME_TIMEOUT));
+		if (!ret) {
+			pr_info("Primary fb resume timeout\n");
+			return -ETIMEDOUT;
+		}
+#ifdef CONFIG_FRAMEBUFFER_CONSOLE
+		console_lock();
+#endif
+		if (!lock_fb_info(prim_fbi)) {
+#ifdef CONFIG_FRAMEBUFFER_CONSOLE
+			console_unlock();
+#endif
+			return -ENODEV;
+		}
+		if (prim_fbi->blank == FB_BLANK_UNBLANK) {
+			unlock_fb_info(prim_fbi);
+#ifdef CONFIG_FRAMEBUFFER_CONSOLE
+			console_unlock();
+#endif
+			return 0;
+		}
+		__pm_stay_awake(&prim_panel_wakelock);
+		ret = fb_blank(prim_fbi, FB_BLANK_UNBLANK);
+		if (!ret) {
+			atomic_set(&prim_panel_is_on, true);
+			if (timeout > 0) {
+				schedule_delayed_work(&prim_panel_work, msecs_to_jiffies(timeout));
+			} else
+				__pm_relax(&prim_panel_wakelock);
+		} else
+			__pm_relax(&prim_panel_wakelock);
+		unlock_fb_info(prim_fbi);
+#ifdef CONFIG_FRAMEBUFFER_CONSOLE
+		console_unlock();
+#endif
+		return ret;
+	}
+
+	pr_err("primary panel is not existed\n");
+	return -EINVAL;
+}
+#endif
 
 
 /*
