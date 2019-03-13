3829d90efdf5d86738921838d0f9c92145c10d3c
Park Ju Hyung <qkrwngud825@gmail.com>
2019-03-13 11:30:02 +0900

    Revert "PM / Suspend: Print wall time at suspend entry and exit"
    This reverts commit b9acbfee678bf41939a9b0bbe09281e53f8ac11a.
    
    This is an expensive logging and not all that useful as it could get skewed.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Change-Id: I187ff808a7487afd7ba39df90360bd6e2165f89f
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -634,7 +634,6 @@ int pm_suspend(suspend_state_t state)
 	if (state <= PM_SUSPEND_ON || state >= PM_SUSPEND_MAX)
 		return -EINVAL;
 
-	gpio_set_value(slst_gpio_base_id + PROC_AWAKE_ID, 0);
 	error = enter_state(state);
 	gpio_set_value(slst_gpio_base_id + PROC_AWAKE_ID, 1);
 	if (error) {
