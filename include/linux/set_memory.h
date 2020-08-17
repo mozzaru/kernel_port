9a44d57ac627c783d3964d448225b0bbf7572e06
Angelo G. Del Regno <kholk11@gmail.com>
2020-08-18 01:56:37 +0530

    Backport new vmalloc for "large performance benefits"
    This is a backport from Linux 5.2-rc1 of a patch series to greatly enhance vmalloc's performance
    especially on embedded systems, plus all of its dependencies that were missing in kernel 4.9.
    
    For all the informations, refer to LKML: https://lkml.org/lkml/2018/10/19/786
    
    Brief informations:
    Currently an allocation of the new VA area is done over busy list iteration until a suitable hole
    is found between two busy areas. Therefore each new allocation causes the list being grown.
    Due to long list and different permissive parameters an allocation can take a long time on
    embedded devices(milliseconds).
    
    This patch organizes the vmalloc memory layout into free areas of the VMALLOC_START-VMALLOC_END range.
    It uses a red-black tree that keeps blocks sorted by their offsets in pair with linked list
    keeping the free space in order of increasing addresses.
    
    Quote Phoronix:
    With this patch from Uladzislau Rezki, calling vmalloc() can take up to 67% less time compared
    to the behavior on Linux 5.1 and prior, at least with tests done by the developer under QEMU.
    
    Personal tests are showing that the device is more responsive when memory pressure is
    high and when huge allocations are to be done, it's also noticeably faster in this case, like
    when starting Chrome with more than 100 opened tabs after a system reboot (so, an
    uncached complete load of it).
    
    Shameless kanged from:
    https://github.com/sonyxperiadev/kernel / Pull Request 2016
    
    (Edited to remove atomic totalram_pages(), as it was causing some
    noticeable performance regressions)
    
    *Note:
    provide linux/set_memory.h
    Currently code that wants to use set_memory_ro() etc, needs to include
    asm/set_memory.h, which doesn't exist on all arches.  Some code knows it
    only builds on arches which have the header, other code guards the
    inclusion with an #ifdef, neither is ideal.
    
    So create linux/set_memory.h.  This always exists, so users don't need
    an #ifdef just to include the header.
    
    When CONFIG_ARCH_HAS_SET_MEMORY=y it includes asm/set_memory.h,
    otherwise it provides empty non-failing implementations.
    
    Link: http://lkml.kernel.org/r/1498717781-29151-1-git-send-email-mpe@ellerman.id.au
    
    Signed-off-by: TogoFire <italomellopereira@gmail.com>
    Change-Id: Id0ca0b31efd7db5781c75867a24f1947d50f092b

--- include/linux/set_memory.h
+++ include/linux/set_memory.h
@@ -9,7 +9,7 @@
 #define _LINUX_SET_MEMORY_H_
 
 #ifdef CONFIG_ARCH_HAS_SET_MEMORY
-#include <asm/set_memory.h>
+#include <asm-generic/set_memory.h>
 #else
 static inline int set_memory_ro(unsigned long addr, int numpages) { return 0; }
 static inline int set_memory_rw(unsigned long addr, int numpages) { return 0; }
