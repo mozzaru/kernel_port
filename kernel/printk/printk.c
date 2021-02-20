909e58209504a7153c5c23a7034bc943dc199c4a
penglezos <panagiotisegl@gmail.com>
2021-02-20 04:31:54 +0000

    kernel: printk: Ignore healthd and other charging spam logs.
    -e 
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    Change-Id: I89673c99131f5ae9127f9d83805a1308a0a83761

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -773,6 +773,8 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 			endp++;
 			len -= endp - line;
 			line = endp;
+			if (strstr(line, "healthd") || strstr(line, "cacert") || !strcmp(line, "CP: Couldn't"))
+			goto free;
 		}
 		}
 	}
@@ -785,6 +787,7 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 	printk_emit(facility, level, NULL, 0, "%s", line);
 ignore:
 	kfree(buf);
+	free:
 	return ret;
 }
 
