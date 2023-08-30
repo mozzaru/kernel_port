c93d25d39a919f0cb0eab4e5f5abf52413424dad
mozzaru <rianyanuar52@gmail.com>
2023-08-30 14:41:30 +0700

    markw: asoc/msm-analog-cdc: add for markw

--- techpack/audio/asoc/codecs/sdm660_cdc/msm-analog-cdc.c
+++ techpack/audio/asoc/codecs/sdm660_cdc/msm-analog-cdc.c
@@ -3193,7 +3193,7 @@ static const struct snd_soc_dapm_route audio_map[] = {
 	{"LINEOUT PA", NULL, "LINE_OUT"},
 	{"LINE_OUT", "Switch", "LINEOUT DAC"},
 	{"LINEOUT DAC", NULL, "PDM_IN_RX3"},
-#ifdef CONFIG_MACH_XIAOMI_C6
+#if defined(CONFIG_MACH_XIAOMI_C6) || defined(CONFIG_MACH_XIAOMI_MARKW)
 	{ "Ext Spk", NULL, "LINEOUT PA"},
 #endif
 
